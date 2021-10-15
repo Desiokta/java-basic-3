0e179cc46185f8ccfaf8d6a19b76d73808baa5cb
desiokta <desiokt10@gmail.com>
2021-10-15 19:14:15 +0700

    Challenge 3

--- JavaBassic3.java
+++ JavaBassic3.java
@@ -1,6 +1,14 @@
-public class JavaBassic3 {
+import java.util.Scanner;
+
+// https://github.com/Desiokta/java-basic-3.git
+
+public class Javabassic3 {
     public static void main(String[] args) {
+        Scanner menu = new Scanner(System.in);
+
         int x = 9;
+        System.out.println("Segitiga");
+
         for (int i = 1; i <= x; i++) {
             for (int j = 8; j >= i; j--) {
                 System.out.print(' ');
